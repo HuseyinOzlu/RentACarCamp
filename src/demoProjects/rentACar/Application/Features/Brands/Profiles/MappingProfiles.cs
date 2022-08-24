b740437afb80abdc1e6ac17711c0ec727d5f29f6
HuseyinOzlu <hozlu509@gmail.com>
2022-08-24 08:28:47 +0300

    2. days

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Brands.Commands.CreateBrand;
+using Application.Features.Brands.Dtos;
+using AutoMapper;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Brand, CreatedBrandDto>().ReverseMap(); //Reverse map çift yönlü
+            CreateMap<Brand, CreateBrandCommand>().ReverseMap(); // Mapleme yapıyoruz
+        }
+    }
+}
